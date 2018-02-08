aacd7f5f89fbbf1f89223d724fe6a7c0e6575586
André Setti <ansetti7@gmail.com>
2018-02-08 16:03:19 -0200

    Working on GUI

--- Main/GUI222.py
+++ Main/GUI222.py
@@ -1,13 +1,9 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'GUI.ui'
-#
-# Created by: PyQt4 UI code generator 4.11.4
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt4 import QtCore, QtGui
 import sys
+import os, webbrowser
+import shutil
+import numpy as np
+from save_data import main
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -22,6 +18,7 @@ try:
 except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
+
 
 class Ui_MainWindow(object):
 
@@ -47,6 +44,94 @@ class Ui_MainWindow(object):
         self.centralwidget.setSizePolicy(sizePolicy)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
 
+        def start_func(self):
+            print('start')
+            #main() doesn't work
+
+        def stop_func(self):
+            print('stop')
+
+
+        def send_func(self):
+            print('send')
+
+
+        def gitHub_func(self):
+            webbrowser.open('https://github.com/Setti7/Stardew-Valley-Fishing-Bot')
+
+        def del_last_data(self):
+
+            choice = QtGui.QMessageBox.question(MainWindow, "Warning!", "Are you sure you want to delete the data from your last fishing?", QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+
+            if choice == QtGui.QMessageBox.Yes:
+
+                try:
+
+                    file_name = 'Data\\training_data.npy'
+                    frames_file = 'Data\\frames.npy'
+
+                    training_data = list(np.load(file_name))
+                    frames = list(np.load(frames_file))
+
+                    before_score = sum(frames)
+
+                    print(len(frames))
+                    print(frames)
+                    print(len(training_data))
+
+                    if len(frames) == 1: #if there is only 1 fishing session, delete all
+
+                        data_folder = os.getcwd()
+                        data_folder_path = os.path.join(data_folder, 'Data')
+
+                        for file in os.listdir(data_folder_path):
+
+                            file_path = os.path.join(data_folder_path, file)
+                            print(file, ' removed')
+                            os.remove(file_path)
+                        
+                        QtGui.QMessageBox.information(MainWindow, "Success", 'All of your data was deleted...')
+                    
+                    else:
+
+                        del frames[-1]
+                        del training_data[-frames[-1]:]
+
+                        np.save(file_name, training_data)
+                        np.save(frames_file, frames)
+
+                        after_score = sum(frames)
+
+                        string = 'Last data was removed successfully!\n\n\tScore before deletion:\t' + str(before_score) + '\n\tYour score now:\t' + str(after_score)
+                        print(string)
+                        QtGui.QMessageBox.information(MainWindow, "Success", string)
+                    
+                except Exception as e:
+                    print(e)
+                    QtGui.QMessageBox.information(MainWindow, "Oops!", "Could not delete the Data")
+
+
+        def del_data(self):
+
+            choice = QtGui.QMessageBox.question(MainWindow, "Warning!", "Are you sure you want to delete all data you gathered? Remember: you can delete just the last one multiple times!", QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+
+            if choice == QtGui.QMessageBox.Yes:
+
+                try:
+                    data_folder = os.getcwd()
+                    data_folder_path = os.path.join(data_folder, 'Data')
+
+                    for file in os.listdir(data_folder_path):
+
+                        file_path = os.path.join(data_folder_path, file)
+                        print(file, ' removed')
+                        os.remove(file_path)
+
+                except Exception as e:
+
+                    print(e)
+                    QtGui.QMessageBox.information(MainWindow, "Oops!", "Could not delete the Data")
+
 
         self.start = QtGui.QPushButton(self.centralwidget)
         self.start.setGeometry(QtCore.QRect(10, 10, 90, 25))
@@ -57,6 +142,8 @@ class Ui_MainWindow(object):
         self.start.setSizePolicy(sizePolicy)
         self.start.setObjectName(_fromUtf8("start"))
 
+        self.start.clicked.connect(start_func)
+        
 
         self.stop = QtGui.QPushButton(self.centralwidget)
         self.stop.setGeometry(QtCore.QRect(150, 10, 90, 25))
@@ -66,6 +153,8 @@ class Ui_MainWindow(object):
         sizePolicy.setHeightForWidth(self.stop.sizePolicy().hasHeightForWidth())
         self.stop.setSizePolicy(sizePolicy)
         self.stop.setObjectName(_fromUtf8("stop"))
+
+        self.stop.clicked.connect(stop_func)
 
 
         self.sendAction = QtGui.QPushButton(self.centralwidget)
@@ -77,10 +166,20 @@ class Ui_MainWindow(object):
         self.sendAction.setSizePolicy(sizePolicy)
         self.sendAction.setObjectName(_fromUtf8("sendAction"))
 
+        self.sendAction.clicked.connect(send_func)
+
+
 
         self.gitHub = QtGui.QPushButton(self.centralwidget)
         self.gitHub.setGeometry(QtCore.QRect(10, 90, 231, 23))
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.gitHub.sizePolicy().hasHeightForWidth())
+        self.gitHub.setSizePolicy(sizePolicy)
         self.gitHub.setObjectName(_fromUtf8("gitHub"))
+
+        self.gitHub.clicked.connect(gitHub_func)
 
         
         MainWindow.setCentralWidget(self.centralwidget)
@@ -90,23 +189,39 @@ class Ui_MainWindow(object):
         self.menuOptions = QtGui.QMenu(self.menubar)
         self.menuOptions.setObjectName(_fromUtf8("menuOptions"))
         MainWindow.setMenuBar(self.menubar)
+
+
         self.statusbar = QtGui.QStatusBar(MainWindow)
         self.statusbar.setObjectName(_fromUtf8("statusbar"))
         MainWindow.setStatusBar(self.statusbar)
+
+
         self.delLastDataAction = QtGui.QAction(MainWindow)
         self.delLastDataAction.setObjectName(_fromUtf8("delLastDataAction"))
+        self.delLastDataAction.setStatusTip('Deletes the data from your last fishing session')
+
+        self.delLastDataAction.triggered.connect(del_last_data)
+
+
         self.delDataAction = QtGui.QAction(MainWindow)
         self.delDataAction.setObjectName(_fromUtf8("delDataAction"))
+        self.delDataAction.setStatusTip('Deletes all the data you gathered. Only do it if you really messed up when collecting data')
+
+        self.delDataAction.triggered.connect(del_data)
+
+
         self.menuOptions.addAction(self.delLastDataAction)
         self.menuOptions.addSeparator()
         self.menuOptions.addAction(self.delDataAction)
         self.menubar.addAction(self.menuOptions.menuAction())
+
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
         MainWindow.setTabOrder(self.start, self.stop)
         MainWindow.setTabOrder(self.stop, self.gitHub)
         MainWindow.setTabOrder(self.gitHub, self.sendAction)
+
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate("MainWindow", "Stardew Valley Fishing Bot", None))
@@ -117,6 +232,8 @@ class Ui_MainWindow(object):
         self.menuOptions.setTitle(_translate("MainWindow", "Options", None))
         self.delLastDataAction.setText(_translate("MainWindow", "Delete Last Data", None))
         self.delDataAction.setText(_translate("MainWindow", "Delete All Data", None))
+
+
 
 
 if __name__ == "__main__":
