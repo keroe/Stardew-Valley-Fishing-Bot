c247520ce05d3e245465c29d608469883055017c
André Setti <ansetti7@gmail.com>
2018-02-11 23:50:28 -0200

    Simplifying

--- STFBSite/mysite/personal/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-	return render(request, 'personal/jumbotron-narrow/index.html')
-
-def contact(request):
-	return render(request, 'personal/basic.html',{'content':['If you would like to contact me, please email me.','ansetti7@gmail.com']})
-
-# Create your views here.
