c861cb81672698a4a97b93f90dcf306dfde7b01d
André Setti <ansetti7@gmail.com>
2018-02-23 04:10:56 -0300

    Fixed some bugs at the upload page

--- mysite/data/views.py
+++ mysite/data/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.db.models import Sum
@@ -9,11 +9,16 @@ from data.forms import UserDataForm
 import numpy as np
 
 def data(request):
-	return render(request, 'data/data_index.html')
+    user_score = UserData.objects.filter(username=request.user.username).aggregate(Sum('score'))['score__sum']
+    return render(request, 'data/data_index.html', {
+        'user': request.user,
+        'user_score': user_score
+    })
 
 def upload(request):
     submitted = ''
     debug = ''
+    user_score = UserData.objects.filter(username=request.user.username).aggregate(Sum('score'))['score__sum']
     if request.method == 'POST':
         form = UserDataForm(request.POST, request.FILES)
         file = request.FILES['file']
@@ -26,12 +31,17 @@ def upload(request):
             last_row.score = len(file)
             last_row.save()
 
-            #total_frames = UserData.objects.aggregate(Sum('score'))
-            total_score = UserData.objects.values('username').annotate('sum__points'=Sum('score'))
-            debug = total_score
+            #total_frames = UserData.objects.aggregate(Sum('score'))["score__sum"]
 
-            submitted = 'True'
-             #'File submitted succesfully. Thank you :)'
+            #user_score = UserData.objects.filter(username=request.user.username).aggregate(Sum('score'))['score__sum']
+            #UsuarioModel = UserProfile.objects.get(user__username=UsuarioElegido)
+            #total_score = UserData.objects.values('username').annotate(Sum('score'))
+
+            #debug = total_frames
+
+            submitted = 'True' #"File submitted succesfully. Thank you :)"
+
+            return redirect('/data/ranking/upload')
             
         else:
             submitted = 'False' #"Your file was not submitted, maybe you are not logged in or your training data is unvalid."
@@ -42,9 +52,16 @@ def upload(request):
     return render(request, 'data/upload_index.html', {
         'form': form,
         'user': request.user,
+        'user_score': user_score,
         'submitted': submitted,
         'debug': debug
     })
 
 def ranking(request):
-	return render(request, 'data/ranking_index.html')
+    user_score = UserData.objects.filter(username=request.user.username).aggregate(Sum('score'))['score__sum']
+    total_score = UserData.objects.aggregate(Sum('score'))["score__sum"]
+    return render(request, 'data/ranking_index.html', {
+        'user': request.user,
+        'user_score': user_score,
+        'total_score': total_score
+    })
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.db.models import Sum
+
+from data.models import UserData
 
 def index(request):
-	return render(request, 'home/index.html')
+	user_score = UserData.objects.filter(username=request.user.username).aggregate(Sum('score'))['score__sum']
+	return render(request, 'home/index.html', {
+        'user': request.user,
+        'user_score': user_score,
+    })
 
 # Create your views here.
